2d18b8a508e7882e21144e498b1554a1780903aa
stephany <by.stephany.dev@gmail.com>
2025-05-27 17:35:16 -0400

    add i18n support for quote component

--- client/src/components/pages/home/quote.tsx
+++ client/src/components/pages/home/quote.tsx
@@ -1,9 +1,15 @@
 import './quote.css';
+import { FormattedMessage } from "react-intl";
 
 const Quote = () => {
   return (
     <div className="quote">
-      <h1>"...working to create a more equitable restaurant ecosystem for all..."</h1>
+      <h1>
+        <FormattedMessage
+          id="home.quote"
+          defaultMessage="...working to create a more equitable restaurant ecosystem for all..."
+        />
+      </h1>
     </div>
   )
 }
