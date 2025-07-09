ff097917b4d94d240901554a9213074ff686b4e9
Angelo Yang <38714377+AngeloHYang@users.noreply.github.com>
2025-07-08 20:29:52 -0400

    header title and text

--- client/src/components/pages/coming-soon/coming-soon.tsx
+++ client/src/components/pages/coming-soon/coming-soon.tsx
@@ -1,15 +1,19 @@
 import PageHeader from "@components/pages/coming-soon/pageheader.tsx";
+import { FormattedMessage } from "react-intl";
 
 const ComingSoon = () => {
-    return (
-        // <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        //     <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
-        //     <p className="text-lg text-gray-600">We're working hard to bring you this feature. Stay tuned!</p>
-        // </div>
-        <main className="coming-soon-page">
-            <PageHeader></PageHeader>
-        </main>
-    );
-}
+  return (
+    // <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    //     <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
+    //     <p className="text-lg text-gray-600">We're working hard to bring you this feature. Stay tuned!</p>
+    // </div>
+    <main className="coming-soon-page">
+      <PageHeader
+        headerTitle={<FormattedMessage id="comingSoon.title" />}
+        headerText={<FormattedMessage id="comingSoon.description" />}
+      ></PageHeader>
+    </main>
+  );
+};
 
 export default ComingSoon;
