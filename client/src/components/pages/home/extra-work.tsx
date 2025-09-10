c058101e0229dcc6349193b8970cff3576c24c1a
Jon Pekkarinen <77648523+jonpekk@users.noreply.github.com>
2025-09-09 21:59:30 -0400

    left aligning text on larger screens

--- client/src/components/pages/home/extra-work.tsx
+++ client/src/components/pages/home/extra-work.tsx
@@ -5,7 +5,7 @@ import ExtraWorkCard from "./extra-work-card";
 const ExtraWork = () => {
   return (
     <div className="extra-work pt-[10px] sm:p-[32px]">
-      <h2 className="text-2xl font-bold text-center">
+      <h2 className="text-2xl font-bold text-center lg:text-start">
         <FormattedMessage id="home.extra-work.h2" />
       </h2>
       <div className="flex flex-col justify-center items-center lg:flex-row lg:flex-wrap lg:justify-start w-full pt-[16px] gap-[64px]">
