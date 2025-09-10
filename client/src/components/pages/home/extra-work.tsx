9fd8b1e9173a580ff721c0f4decf4672294fd286
Jon Pekkarinen <77648523+jonpekk@users.noreply.github.com>
2025-09-09 20:46:59 -0400

    bugfix: adjusting extra work layout to ensure responsiveness

--- client/src/components/pages/home/extra-work.tsx
+++ client/src/components/pages/home/extra-work.tsx
@@ -3,18 +3,18 @@ import { extraWorkData } from "@/data/extra-work-data";
 import ExtraWorkCard from "./extra-work-card";
 
 const ExtraWork = () => {
-	return (
-		<div className="extra-work p-[32px] overflow-x-scroll">
-			<h2 className="text-2xl font-bold">
-            <FormattedMessage id="home.extra-work.h2" />
-         </h2>
-			<div className="flex w-full pt-[16px] gap-x-[64px]">
-				{extraWorkData.map((item, index) => (
-					<ExtraWorkCard key={index} item={item}/>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="extra-work p-[32px]">
+      <h2 className="text-2xl font-bold">
+        <FormattedMessage id="home.extra-work.h2" />
+      </h2>
+      <div className="flex flex-col lg:flex-row lg:flex-wrap w-full pt-[16px] gap-[64px]">
+        {extraWorkData.map((item, index) => (
+          <ExtraWorkCard key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ExtraWork;
