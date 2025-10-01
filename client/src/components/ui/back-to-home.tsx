78b6dabf57cb37eb1d565a1b1dd27e42476c37a2
Hanpeng Yang <38714377+AngeloHYang@users.noreply.github.com>
2025-09-30 20:46:53 -0400

    188 back to home buttons clickable area extends beyond its visible bounds (#207)

--- client/src/components/ui/back-to-home.tsx
+++ client/src/components/ui/back-to-home.tsx
@@ -4,34 +4,32 @@ import backToHomeStyles from "./back-to-home.module.css";
 
 function BackToHome() {
   return (
-    <div>
-      <Link
-        to="/"
-        className={`
-          flex
-          justify-center
-          items-center
-          gap-[10px]
-          w-[311px]
-          px-[24px] py-[8px]
-          border-[1px]
-          border-black
-          rounded-[8px]
-          bg-light
-          hover:bg-[#EAEAEA]
-          active:bg-[#CDCDCD]
-          !text-dark
-          !hover:text-[#383838]
-          !active:text-[#404040]
-          font-bold
-          transition-all
-          duration-200
-          ${backToHomeStyles.backToHome}
-        `}
-      >
-        <FormattedMessage id="ui.backToHomeButton" />
-      </Link>
-    </div>
+    <Link
+      to="/"
+      className={`
+        flex
+        justify-center
+        items-center
+        gap-[10px]
+        w-[311px]
+        px-[24px] py-[8px]
+        border-[1px]
+        border-black
+        rounded-[8px]
+        bg-light
+        hover:bg-[#EAEAEA]
+        active:bg-[#CDCDCD]
+        !text-dark
+        !hover:text-[#383838]
+        !active:text-[#404040]
+        font-bold
+        transition-all
+        duration-200
+        ${backToHomeStyles.backToHome}
+    `}
+    >
+      <FormattedMessage id="ui.backToHomeButton" />
+    </Link>
   );
 }
 
