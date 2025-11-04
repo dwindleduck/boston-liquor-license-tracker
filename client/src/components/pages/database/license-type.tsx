4549b3055ca6415058a945c52dfa37b17206e460
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2025-11-04 13:45:32 -0500

    font sizes for body text

--- client/src/components/pages/database/license-type.tsx
+++ client/src/components/pages/database/license-type.tsx
@@ -4,34 +4,34 @@ import { FormattedMessage } from "react-intl";
 const LicenseType = () => {
   return (
     <section className={styles.licenseType}>
-      <h2 className="font-bold text-[36px] m-0">
+      <h2 className="font-bold m-0">
         <FormattedMessage id="database.licenseTerminology.header" />
       </h2>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
+      <p className="mt-[8px] mb-[16px] font-normal">
         <FormattedMessage id="database.licenseTerminology.description" />
       </p>
-      <h4 className="font-medium text-[24px]">
+      <h4 className="font-medium">
         <FormattedMessage id="database.licenseTerminology.allAlcohol.title" />
       </h4>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
+      <p className="mt-[8px] mb-[16px] font-normal">
         <FormattedMessage id="database.licenseTerminology.allAlcohol.description" />
       </p>
-      <h4 className="font-medium text-[24px]">
+      <h4 className="font-medium">
         <FormattedMessage id="database.licenseTerminology.wineAndBeer.title" />
       </h4>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
+      <p className="mt-[8px] mb-[16px] font-normal">
         <FormattedMessage id="database.licenseTerminology.wineAndBeer.description" />
       </p>
-      <h4 className="font-medium text-[24px]">
+      <h4 className="font-medium">
         <FormattedMessage id="database.licenseTerminology.transferable.title" />
       </h4>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
+      <p className="mt-[8px] mb-[16px] font-normal">
         <FormattedMessage id="database.licenseTerminology.transferable.description" />
       </p>
-      <h4 className="font-medium text-[24px]">
+      <h4 className="font-medium">
         <FormattedMessage id="database.licenseTerminology.nonTransferable.title" />
       </h4>
-      <p className="mt-[8px] mb-[16px] font-normal text-[16px]">
+      <p className="mt-[8px] mb-[16px] font-normal">
         <FormattedMessage id="database.licenseTerminology.nonTransferable.description" />
       </p>
     </section>
