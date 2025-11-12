edab2d35b6c5a8b73ee2faf38c0a20ee3faea8ab
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2025-11-12 07:49:07 -0500

    fix button nesting issue

--- client/src/components/pages/home/navigation-button.tsx
+++ client/src/components/pages/home/navigation-button.tsx
@@ -8,24 +8,21 @@ interface NavigationButtonProps {
 
 const NavigationButton = ({ to, messageId }: NavigationButtonProps) => {
   return (
-    <Link to={to}>
-      <button className="
-                flex
-                justify-center
-                items-center
-                text-center
-                w-full md:w-[400px]
-                min-h-[34px] md:min-h-[62px]
-                px-[24px] py-[8px] md:py-[12px]
-                rounded-[8px]
-                bg-button-default-dark hover:bg-button-hovered-dark active:bg-button-active-dark
-                text-button-default-light hover:text-button-hovered-light active:text-button-active-light
-                text-[15px] md:text-[32px] font-normal
-                cursor-pointer
-            "
-      >
-        <FormattedMessage id={messageId} />
-      </button>
+    <Link to={to} className="
+      flex
+      justify-center
+      items-center
+      text-center
+      w-full md:w-[400px]
+      min-h-[34px] md:min-h-[62px]
+      px-[24px] py-[8px] md:py-[12px]
+      rounded-[8px]
+      bg-button-default-dark hover:bg-button-hovered-dark active:bg-button-active-dark
+      text-button-default-light hover:text-button-hovered-light active:text-button-active-light
+      text-[15px] md:text-[32px] font-normal
+      cursor-pointer
+    ">
+      <FormattedMessage id={messageId} />
     </Link>
   )
 }
--- client/src/components/pages/home/read-study-button.tsx
+++ client/src/components/pages/home/read-study-button.tsx
@@ -9,29 +9,24 @@ interface ReadStudyButtonProps {
 
 const ReadStudyButton = ({ to, messageId }: ReadStudyButtonProps) => {
   return (
-    < Link to={to} >
-      <button
-        className="
-                inline-flex
-                justify-center
-                items-center
-                bg-case-study-button-default-yellow
-                hover:bg-case-study-button-hover-yellow
-                active:bg-case-study-button-active-yellow
-                text-font-dark
-                hover:text-button-active-dark
-                active:text-[--color-button-hovered-dark]
-                font-bold
-                px-8
-                py-4
-                rounded-lg shadow-md
-                gap-2.5
-                "
-      >
-        <FormattedMessage
-          id={messageId}
-        />
-      </button>
+    < Link to={to} className="
+      inline-flex
+      justify-center
+      items-center
+      bg-case-study-button-default-yellow
+      hover:bg-case-study-button-hover-yellow
+      active:bg-case-study-button-active-yellow
+      text-font-dark
+      hover:text-button-active-dark
+      active:text-button-hovered-dark
+      font-bold
+      px-8
+      py-4
+      rounded-lg shadow-md
+      gap-2.5
+      "
+    >
+      <FormattedMessage id={messageId} />
     </Link >
   )
 }
