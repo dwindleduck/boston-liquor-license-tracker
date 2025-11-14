96ac0ef637ef313a916800f55801d3ace5584970
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2025-11-14 15:29:39 -0500

    convert database page

--- client/src/components/pages/database/license-type.tsx
+++ client/src/components/pages/database/license-type.tsx
@@ -4,34 +4,34 @@ import { FormattedMessage } from "react-intl";
 const LicenseType = () => {
   return (
     <section className={styles.licenseType}>
-      <h2 className="font-bold">
+      <h2>
         <FormattedMessage id="database.licenseTerminology.header" />
       </h2>
-      <p className="font-normal">
+      <p>
         <FormattedMessage id="database.licenseTerminology.description" />
       </p>
-      <h3 className="font-medium">
+      <h3>
         <FormattedMessage id="database.licenseTerminology.allAlcohol.title" />
       </h3>
-      <p className="font-normal">
+      <p>
         <FormattedMessage id="database.licenseTerminology.allAlcohol.description" />
       </p>
-      <h3 className="font-medium">
+      <h3>
         <FormattedMessage id="database.licenseTerminology.wineAndBeer.title" />
       </h3>
-      <p className="font-normal">
+      <p>
         <FormattedMessage id="database.licenseTerminology.wineAndBeer.description" />
       </p>
-      <h3 className="font-medium">
+      <h3>
         <FormattedMessage id="database.licenseTerminology.transferable.title" />
       </h3>
-      <p className="font-normal">
+      <p>
         <FormattedMessage id="database.licenseTerminology.transferable.description" />
       </p>
-      <h3 className="font-medium">
+      <h3>
         <FormattedMessage id="database.licenseTerminology.nonTransferable.title" />
       </h3>
-      <p className="font-normal">
+      <p>
         <FormattedMessage id="database.licenseTerminology.nonTransferable.description" />
       </p>
     </section>
