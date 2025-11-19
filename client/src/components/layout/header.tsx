531f8b62bd13ead897599a70c2426d69812aab6c
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2025-11-18 22:48:55 -0500

    spacing updates

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -10,7 +10,7 @@ import logoPressed from "@/assets/logo_pressed.svg";
 
 const Spacer = () => <span className="mx-4">&bull;</span>;
 
-const LineSpacer = () => <div className="w-[160px] h-px bg-background-light mx-4" />;
+const LineSpacer = () => <div className="w-[160px] h-px bg-background-light" />;
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,70 +34,68 @@ const Header = () => {
     <header
       className={`${styles.siteHeader} bg-button-default-dark shadow-md sticky top-0 left-0 w-full z-50`}
     >
-      <div className="flex max-w-7xl sm:px-[24px] py-[18px] md:p-6 lg:items-center text-center">
-        <nav className="flex w-full items-center">
-          <Link
-            to="/"
-            className="text-xl font-bold"
-            onClick={handleLogoClick}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <img
-              className={styles.logoImage}
-              src={logoSrc}
-              alt={intl.formatMessage({ id: "header.logo-altText" })}
-            />
+      <nav className="flex w-full items-center">
+        <Link
+          to="/"
+          className="text-xl font-bold"
+          onClick={handleLogoClick}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img
+            className={styles.logoImage}
+            src={logoSrc}
+            alt={intl.formatMessage({ id: "header.logo-altText" })}
+          />
+        </Link>
+
+        <nav className="hidden md:flex items-center w-full">
+          <Spacer />
+          <Link to="/maps">
+            <FormattedMessage id="shared.map.capitalized" />
+          </Link>
+          <Spacer />
+          <Link to="/database">
+            <FormattedMessage id="shared.database.capitalized" />
+          </Link>
+          <Spacer />
+          <Link to="/resources">
+            <FormattedMessage id="shared.resources.capitalized" />
           </Link>
 
-          <div className="hidden md:flex items-center w-full">
-            <Spacer />
-            <Link to="/maps">
-              <FormattedMessage id="shared.map.capitalized" />
-            </Link>
-            <Spacer />
-            <Link to="/database">
-              <FormattedMessage id="shared.database.capitalized" />
-            </Link>
-            <Spacer />
-            <Link to="/resources">
-              <FormattedMessage id="shared.resources.capitalized" />
-            </Link>
-
-            <div className="absolute right-6 top-1/2 -translate-y-1/2 flex items-center">
-              <img
-                src={language}
-                className={"inline-block size-[20px] text-font-light me-2"}
-                alt={intl.formatMessage({ id: "header.language" })}
-              />
-              <LangSwitcher />
-            </div>
-          </div>
-
-          <div className="flex md:hidden ml-auto">
-            <button onClick={toggleMenu} aria-label="Toggle menu">
-              <span className="justify-end material-icons">
-                {isOpen ? "close" : "menu"}
-              </span>
-            </button>
+          <div className="absolute right-6 top-1/2 -translate-y-1/2 flex items-center">
+            <img
+              src={language}
+              className={"inline-block size-[20px] text-font-light"}
+              alt={intl.formatMessage({ id: "header.language" })}
+            />
+            <LangSwitcher />
           </div>
         </nav>
-      </div>
+
+        <div className="flex md:hidden ml-auto">
+          <button onClick={toggleMenu} aria-label="Toggle menu">
+            <span className="justify-end material-icons">
+              {isOpen ? "close" : "menu"}
+            </span>
+          </button>
+        </div>
+      </nav>
 
       {isOpen && (
-        <nav className="md:hidden px-4 bg-background-dark">
-          <Link to="/maps" className="py-[8px] px-[24px] block">
+        <nav className="md:hidden bg-background-dark mt-4">
+          <Link to="/maps" className="py-[8px] block">
             <FormattedMessage id="shared.map.capitalized" />
           </Link>
           <LineSpacer />
-          <Link to="/database" className="py-[8px] px-[24px] block">
+          <Link to="/database" className="py-[8px] block">
             <FormattedMessage id="shared.database.capitalized" />
           </Link>
           <LineSpacer />
-          <Link to="/resources" className="py-[8px] px-[24px] block">
+          <Link to="/resources" className="py-[8px] block">
             <FormattedMessage id="shared.resources.capitalized" />
           </Link>
-          <div className="py-[16px] px-[24px] pb-[8px] ms-auto">
+          <div className="py-[16px] pb-[8px] ms-auto">
             <img
               src={language}
               className={"inline-block size-[20px] text-font-light me-2"}
