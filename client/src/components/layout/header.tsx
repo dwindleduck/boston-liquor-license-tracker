4cbea5b685f3b860b12e7a820ad2aea7ae0aca64
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2025-11-21 09:19:39 -0500

    Nav styling (#291)
    Addresses issue #136 for cleaning up the site header styling and
    behavior. This confirms the mobile responsiveness of the header, and
    makes changes to the logo state management.

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -10,23 +10,17 @@ import logoPressed from "@/assets/logo_pressed.svg";
 
 const Spacer = () => <span className="mx-4">&bull;</span>;
 
-const LineSpacer = () => <div className="w-[160px] h-px bg-background-light mx-4" />;
+const LineSpacer = () => <div className="w-[160px] h-px bg-background-light" />;
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
-  const [logoClicked, setLogoClicked] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // Hamburger Menu isOpen
+  const [logoSrc, setLogoSrc] = useState(logoDefault); // Logo isHovered
 
-  const toggleMenu = () => setIsOpen(!isOpen);
-  const handleMouseEnter = () => setIsHovered(true);
-  const handleMouseLeave = () => setIsHovered(false);
-  const handleLogoClick = () => setLogoClicked(true);
-
-  const logoSrc = isHovered
-    ? logoHover
-    : logoClicked
-      ? logoPressed
-      : logoDefault;
+  const toggleMenu = () => setIsOpen(!isOpen); // Hamburger Menu Toggle
+  const handleMouseEnter = () => setLogoSrc(logoHover) // Logo Hover
+  const handleMouseLeave = () => setLogoSrc(logoDefault); // Logo Default
+  const handleMouseDown = () => setLogoSrc(logoPressed) // Logo Click
+  const handleMouseUp = () => setLogoSrc(logoHover) // Logo Click Release
 
   const intl = useIntl();
 
@@ -34,70 +28,70 @@ const Header = () => {
     <header
       className={`${styles.siteHeader} bg-button-default-dark shadow-md sticky top-0 left-0 w-full z-50`}
     >
-      <div className="flex max-w-7xl sm:px-[24px] py-[18px] md:p-6 lg:items-center text-center">
-        <nav className="flex w-full items-center">
-          <Link
-            to="/"
-            className="text-xl font-bold"
-            onClick={handleLogoClick}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <img
-              className={styles.logoImage}
-              src={logoSrc}
-              alt={intl.formatMessage({ id: "header.logo-altText" })}
-            />
+      <section className="flex">
+        <Link
+          to="/"
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className="flex items-center"
+        >
+          <img
+            className={styles.logoImage}
+            src={logoSrc}
+            alt={intl.formatMessage({ id: "header.logo-altText" })}
+          />
+        </Link>
+
+        {/* Non-mobile Nav & Language Switcher */}
+        <nav className="hidden md:flex items-center text-center w-full">
+          <Spacer />
+          <Link to="/maps">
+            <FormattedMessage id="shared.map.capitalized" />
+          </Link>
+          <Spacer />
+          <Link to="/database">
+            <FormattedMessage id="shared.database.capitalized" />
+          </Link>
+          <Spacer />
+          <Link to="/resources">
+            <FormattedMessage id="shared.resources.capitalized" />
           </Link>
 
-          <div className="hidden md:flex items-center w-full">
-            <Spacer />
-            <Link to="/maps">
-              <FormattedMessage id="shared.map.capitalized" />
-            </Link>
-            <Spacer />
-            <Link to="/database">
-              <FormattedMessage id="shared.database.capitalized" />
-            </Link>
-            <Spacer />
-            <Link to="/resources">
-              <FormattedMessage id="shared.resources.capitalized" />
-            </Link>
-
-            <div className="absolute right-6 top-1/2 -translate-y-1/2 flex items-center">
-              <img
-                src={language}
-                className={"inline-block size-[20px] text-font-light me-2"}
-                alt={intl.formatMessage({ id: "header.language" })}
-              />
-              <LangSwitcher />
-            </div>
-          </div>
-
-          <div className="flex md:hidden ml-auto">
-            <button onClick={toggleMenu} aria-label="Toggle menu">
-              <span className="justify-end material-icons">
-                {isOpen ? "close" : "menu"}
-              </span>
-            </button>
+          <div className="flex ml-auto pl-1 mr-5 items-center">
+            <img
+              src={language}
+              className={"inline-block size-[20px] text-font-light"}
+              alt={intl.formatMessage({ id: "header.language" })}
+            />
+            <LangSwitcher />
           </div>
         </nav>
-      </div>
 
+        {/* Hamburger Toggle */}
+        <button className="flex md:hidden ml-auto" onClick={toggleMenu} aria-label="Toggle menu">
+          <span className="justify-end material-icons">
+            {isOpen ? "close" : "menu"}
+          </span>
+        </button>
+      </section>
+
+      {/* Mobile Nav & Language Switcher*/}
       {isOpen && (
-        <nav className="md:hidden px-4 bg-background-dark">
-          <Link to="/maps" className="py-[8px] px-[24px] block">
+        <nav className="md:hidden bg-background-dark mt-4">
+          <Link to="/maps" className="py-[8px] block">
             <FormattedMessage id="shared.map.capitalized" />
           </Link>
           <LineSpacer />
-          <Link to="/database" className="py-[8px] px-[24px] block">
+          <Link to="/database" className="py-[8px] block">
             <FormattedMessage id="shared.database.capitalized" />
           </Link>
           <LineSpacer />
-          <Link to="/resources" className="py-[8px] px-[24px] block">
+          <Link to="/resources" className="py-[8px] block">
             <FormattedMessage id="shared.resources.capitalized" />
           </Link>
-          <div className="py-[16px] px-[24px] pb-[8px] ms-auto">
+          <div className="pt-[16px] ms-auto">
             <img
               src={language}
               className={"inline-block size-[20px] text-font-light me-2"}
