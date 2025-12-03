88262a71be4def3bc4c9e841faf1274c4a8a8db5
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2025-12-03 07:52:44 -0500

    button component cleanup tailwind

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -5,22 +5,26 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-background-dark focus-visible:ring-background-dark/50 focus-visible:ring-[3px]"
+  // For Design: need to define an aria-invalid state for buttons
+  // aria-invalid:ring-<new-color-variable>/20 aria-invalid:border-<new-color-variable>
+  ,
   {
     variants: {
       variant: {
         default:
-          "bg-(--color-background-dark) hover:bg-(--color-button-hovered-dark) active:bg-(--color-button-active-dark) text-(--color-font-light) hover:text-(--color-font-text-links-hover) active:text-(--color-font-text-links-active) rounded-[8px] cursor-pointer w-[200px] text-[18px] font-medium",
-        // The below variants aren't finalized, feel free to make changes when needed
-        destructive:
-          "bg-destructive text-font-light shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
-        outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-background-dark hover:bg-button-hovered-dark active:bg-button-active-dark text-font-light hover:text-font-text-links-hover active:text-font-text-links-active rounded-[8px] cursor-pointer w-[200px] text-[18px] font-medium",
+        // We have not implemented button variants yet, these are suggested from shadcn:
+        // destructive:
+        //   "",
+        // outline:
+        //   "",
+        // secondary:
+        //   "",
+        // ghost:
+        //   "",
+        // link: 
+        //   "",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
