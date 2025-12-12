4e2c798b9b2798f0ebdaaba4e51912d549e570bf
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2025-12-12 17:14:34 -0500

    clean up nested div structure and class names

--- client/src/components/pages/home/extra-work-card.tsx
+++ client/src/components/pages/home/extra-work-card.tsx
@@ -1,5 +1,6 @@
 import { ExtraWork } from "@/data/extra-work-data";
 import { useIntl } from "react-intl";
+import styles from "./extra-work-card.module.css";
 
 interface ExtraWorkCardProps {
   item: ExtraWork;
@@ -8,34 +9,50 @@ interface ExtraWorkCardProps {
 const ExtraWorkCard = ({ item }: ExtraWorkCardProps) => {
   const intl = useIntl();
   return (
-    <div className="shrink-0 w-full md:size-[320px] rounded-[9px] bg-background-dark  ">
-      <a
-        href={item.href}
-        target="_blank"
+    <a
+      href={item.href}
+      target="_blank"
+      className={`
+                ${item.name}
+                flex
+                items-end
+                shrink-0
+                w-full
+                size-full
+                md:size-[320px]
+                min-h-[96px]
+
+                py-[12px] px-[16px] md:py-[16px]
+                
+                rounded-[8px]
+                
+                bg-background-dark
+                bg-cover
+                bg-center
+                bg-no-repeat
+                
+                cursor-pointer
+                hover:opacity-90
+                active:opacity-70
+                
+                focus-visible:border-border-gray
+                focus-visible:ring-border-gray
+                focus-visible:ring-[8px]
+            `}
+      style={{ backgroundImage: `url(${item.imgSrc})` }}
+      title={intl.formatMessage({ id: item.alt })}
+    >
+      <p
         className={`
-                  flex
-                  items-end
-                  size-full
-                  min-h-[96px]
-                  py-[12px] px-[16px] md:py-[16px]
-                  rounded-[8px]
-                  bg-cover
-                  bg-center
-                  bg-no-repeat
-                  cursor-pointer
-                  hover:opacity-90
-                  active:opacity-70
-              `}
-        style={{ backgroundImage: `url(${item.imgSrc})` }}
-        title={intl.formatMessage({ id: item.alt })}
+          text-[14px]
+          md:text-[32px]
+          text-shadow-lg
+          font-medium
+          ${item.colorTheme === "light" ? "text-font-dark" : "text-font-light"}`}
       >
-        <p
-          className={`text-[14px] md:text-[32px] text-shadow-lg font-medium ${item.textColor === "light" ? "text-font-dark" : "text-font-light"}`}
-        >
-          {item.title}
-        </p>
-      </a>
-    </div>
+        {item.title}
+      </p>
+    </a>
   );
 };
 
