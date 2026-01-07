893890fd1f7d531eaf771f7242fc34f7aa6995c4
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2026-01-07 14:11:39 -0500

    fix import issue

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import eslintLocalRules from './eslint-local-rules'
+import eslintLocalRules from './eslint-local-rules.js'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
