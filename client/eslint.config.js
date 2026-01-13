589ee57043b170e15af29dfbd5c702f9e1f2a588
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2026-01-13 07:08:59 -0500

    import path

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import eslintLocalRules from './eslint-local-rules.js'
 import tseslint from 'typescript-eslint'
+import path from 'path'
 
 export default tseslint.config(
   { ignores: ['dist'] },
