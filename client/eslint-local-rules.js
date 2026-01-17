db4aa80ae454eb549a029dd465f904b85b589ad0
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2026-01-17 14:30:43 -0500

    Linting to disallow hex colors outside of global styles file  (#334)
    _Committed directly to a branch of the main repo, which gets us around
    the permissions issues seen when committing from a fork._
    
    This PR uses `eslint-local-rules` and `stylelint` to scour the codebase
    for hex codes.
    
    This has been tested on [Andrew's
    fork](https://github.com/dwindleduck/boston-liquor-license-tracker/pull/4).
    
    Two lines have been left in for testing and will be changed once this is
    tested on the main repo.

---
+++ client/eslint-local-rules.js
@@ -0,0 +1,51 @@
+const eslinstLocalRules = {
+  rules:{
+    'no-hex-colors': {
+      meta: {
+        type: 'problem',
+        docs: {
+          description: 'Disallow hex color codes outside of src/styles/index.css',
+          category: 'Best Practices'
+        },
+        messages: {
+          noHexColors: 'Hex color codes are only allowed in client/src/styles/index.css. Use Tailwind classes or CSS variables instead. See client/README.md#using-tailwind-utility-classes'
+        }
+      },
+      create(context) {
+        // Only run this rule if we're NOT in the global styles file
+        const filename = context.getFilename();
+        if (filename.endsWith('src/styles/index.css')) {
+          return {};
+        }
+
+        const hexColorRegex = /#[0-9A-Fa-f]{3,8}\b/g;
+
+        return {
+          // Because regular strings ('#FF0000') and template strings (`#FF0000`)
+          // are different node types in the AST, 
+          // we need to check both to catch all hex colors.
+          Literal(node) {
+            const value = node.value;
+            if (typeof value === 'string' && hexColorRegex.test(value)) {
+              context.report({
+                node,
+                messageId: 'noHexColors'
+              });
+            }
+          },
+          TemplateElement(node) {
+            const value = node.value.raw;
+            if (hexColorRegex.test(value)) {
+              context.report({
+                node,
+                messageId: 'noHexColors'
+              });
+            }
+          }
+        };
+      }
+    }
+  }
+};
+
+export default eslinstLocalRules;
